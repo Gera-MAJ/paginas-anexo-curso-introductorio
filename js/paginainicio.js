4550b3000c698e24b06ba9df9ead205c82048eff
gera-maj <jatipg@gmail.com>
2023-01-10 03:34:57 -0300

    scroll y mas javascript

--- js/paginainicio.js
+++ js/paginainicio.js
@@ -1,3 +1,14 @@
+const scrollear = window.scrollY
+
+if (scrollear > 0){
+    window.scroll({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    })
+}
+
+
 window.onload = function(){
     
     let conteiner = document.getElementById("conteiner-charge")
